e1264385be22364480aeb7a0bf6c86e630b7f219
Jason K. Moore <moorepants@gmail.com>
2024-12-10 08:58:59 +0100

    Added function to extract the trial start and stop times.

--- src/data.py
+++ src/data.py
@@ -1,7 +1,8 @@
 import os
 
-import yaml
+import numpy as np
 import pandas as pd
+import yaml
 
 with open('config.yml') as f:
     config_data = yaml.safe_load(f)
@@ -85,10 +86,46 @@ def merge_imu_data_frames(*data_frames):
     return merged
 
 
+def load_trial_bounds(path, session_label):
+    """Returns a data frame that has a row for each trial in a session along
+    with its start and stop indices.
+
+Surface,BotTrike,SeatBot,SeatHead,RearWheel,FrontWheel
+static,"[148116, 151684]",,,,
+static,,"[156665, 160216]",,,
+static,,,"[161435, 165003]",,
+static,,,,"[119249, 122815]",
+static,,,,,"[124468, 128020]"
+Aula,"[65784, 83246]",,,,
+    """
+    df = pd.read_csv(path, header=0)
+    # only use the rear wheel indices
+    df = df[['Surface', 'RearWheel']].dropna()
+    # Index,Surface,RearWheel
+    # 0,static,"[119249, 122815]"
+    df.index = range(len(df))
+    df[['start_idx', 'stop_idx']] = df['RearWheel'].str.split(',', expand=True)
+    df['start_idx'] = df['start_idx'].str.replace('[', '').astype(int)
+    df['stop_idx'] = df['stop_idx'].str.replace(']', '').astype(int)
+    with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
+        sessions = yaml.safe_load(f)
+    filename = sessions[session_label]['imu_files']['rear_wheel']
+    path_to_file = os.path.join(PATH_TO_DATA, 'Raw_data_csv', filename)
+    rw_df = load_shimmer_file(path_to_file)
+    df['start_time'] = rw_df.index[:len(df)]
+    df['stop_time'] = rw_df.index[:len(df)]
+    for idx, row in df.iterrows():
+        df.loc[idx, 'start_time'] = rw_df.index.values[row['start_idx']]
+        df.loc[idx, 'stop_time'] = rw_df.index.values[row['stop_idx']]
+    return df
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    dfs = load_session_files('session001')
+    session_label = 'session001'
+
+    dfs = load_session_files(session_label)
 
     #dfs['front_wheel'].plot(subplots=True)
     #dfs['rear_wheel'].plot(subplots=True)
@@ -100,14 +137,22 @@ if __name__ == "__main__":
 
     # NOTE : The missing values (NANs) cause this plot to show gaps. I think it
     # is simply a consequence of plotting the NANs, but it is a bit confusing.
-    merged.plot(subplots=True, linestyle='-')
+    #merged.plot(subplots=True, linestyle='-')
     #merged.plot(subplots=True, marker='.')
 
     # NOTE : This fills all NANs with linear interpolation based on the time
     # index.
     # TODO : check if there is a difference in method='time' or 'index'
     #interpolated = merged.interpolate(method='time')
-    interpolated = merged.interpolate(method='index')
-    interpolated.plot(subplots=True)
+    #interpolated = merged.interpolate(method='index')
+    #interpolated.plot(subplots=True)
+
+    with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
+        sessions = yaml.safe_load(f)
+    filename = sessions[session_label]['trial_bounds_file']
+    path_to_file = os.path.join(PATH_TO_DATA, 'Interval_indexes', filename)
+    bounds_df = load_trial_bounds(path_to_file, session_label)
+    static = merged[bounds_df.loc[0, 'start_time']:bounds_df.loc[0, 'stop_time']]
+    static.interpolate(method='time').plot(subplots=True)
 
     plt.show()
