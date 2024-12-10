daa347c07dda288a9f0e8051f25cd3b1d5f2deb2
Jason K. Moore <moorepants@gmail.com>
2024-12-10 13:14:02 +0100

    Added a basic Session class.

--- src/data.py
+++ src/data.py
@@ -12,6 +12,53 @@ PATH_TO_REPO = os.path.realpath(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
 
+class Session():
+    def __init__(self, session_label):
+
+        self.session_label = session_label
+
+        with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
+            sessions = yaml.safe_load(f)
+
+        self.meta_data = sessions[session_label]
+
+    def load_data(self):
+
+        path_to_bounds_file = os.path.join(PATH_TO_DATA, 'Interval_indexes',
+                                           self.meta_data['trial_bounds_file'])
+
+        self.imu_data_frames = load_session_files(self.session_label)
+        self.bounds_data_frame = load_trial_bounds(
+            path_to_bounds_file, self.imu_data_frames['rear_wheel'].index)
+
+    def merge_data(self):
+
+        try:
+            self.imu_data_frames
+        except AttributeError:
+            self.load_data()
+
+        self.raw_data = merge_imu_data_frames(*self.imu_data_frames.values())
+
+    def extract_trial(self, trial_name, trial_number=0):
+
+        try:
+            self.raw_data
+        except AttributeError:
+            self.merge_data()
+
+        count = 0
+        for idx, row in self.bounds_data_frame.iterrows():
+            if row['Surface'] == trial_name:
+                if trial_number == count:
+                    start_idx = row['start_time']
+                    stop_idx = row['stop_time']
+                    break
+                count += 1
+
+        return self.raw_data[start_idx:stop_idx]
+
+
 def load_shimmer_file(path):
     """Loads a single Shimmer aquisition csv file into Pandas data frame.
 
@@ -86,7 +133,7 @@ def merge_imu_data_frames(*data_frames):
     return merged
 
 
-def load_trial_bounds(path, session_label):
+def load_trial_bounds(path, rear_wheel_timestamp):
     """Returns a data frame that has a row for each trial in a session along
     with its start and stop indices.
 
@@ -107,16 +154,13 @@ Aula,"[65784, 83246]",,,,
     df[['start_idx', 'stop_idx']] = df['RearWheel'].str.split(',', expand=True)
     df['start_idx'] = df['start_idx'].str.replace('[', '').astype(int)
     df['stop_idx'] = df['stop_idx'].str.replace(']', '').astype(int)
-    with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
-        sessions = yaml.safe_load(f)
-    filename = sessions[session_label]['imu_files']['rear_wheel']
-    path_to_file = os.path.join(PATH_TO_DATA, 'Raw_data_csv', filename)
-    rw_df = load_shimmer_file(path_to_file)
-    df['start_time'] = rw_df.index[:len(df)]
-    df['stop_time'] = rw_df.index[:len(df)]
+
+    df['start_time'] = rear_wheel_timestamp[:len(df)]
+    df['stop_time'] = rear_wheel_timestamp[:len(df)]
+
     for idx, row in df.iterrows():
-        df.loc[idx, 'start_time'] = rw_df.index.values[row['start_idx']]
-        df.loc[idx, 'stop_time'] = rw_df.index.values[row['stop_idx']]
+        df.loc[idx, 'start_time'] = rear_wheel_timestamp.values[row['start_idx']]
+        df.loc[idx, 'stop_time'] = rear_wheel_timestamp.values[row['stop_idx']]
     return df
 
 
@@ -125,7 +169,7 @@ if __name__ == "__main__":
 
     session_label = 'session001'
 
-    dfs = load_session_files(session_label)
+    #dfs = load_session_files(session_label)
 
     #dfs['front_wheel'].plot(subplots=True)
     #dfs['rear_wheel'].plot(subplots=True)
@@ -133,7 +177,7 @@ if __name__ == "__main__":
     #dfs['trike_bottom'].plot(subplots=True)
     #dfs['seat_head'].plot(subplots=True)
 
-    merged = merge_imu_data_frames(*dfs.values())
+    #merged = merge_imu_data_frames(*dfs.values())
 
     # NOTE : The missing values (NANs) cause this plot to show gaps. I think it
     # is simply a consequence of plotting the NANs, but it is a bit confusing.
@@ -147,12 +191,16 @@ if __name__ == "__main__":
     #interpolated = merged.interpolate(method='index')
     #interpolated.plot(subplots=True)
 
-    with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
-        sessions = yaml.safe_load(f)
-    filename = sessions[session_label]['trial_bounds_file']
-    path_to_file = os.path.join(PATH_TO_DATA, 'Interval_indexes', filename)
-    bounds_df = load_trial_bounds(path_to_file, session_label)
-    static = merged[bounds_df.loc[0, 'start_time']:bounds_df.loc[0, 'stop_time']]
-    static.interpolate(method='time').plot(subplots=True)
+    #with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
+        #sessions = yaml.safe_load(f)
+    #filename = sessions[session_label]['trial_bounds_file']
+    #path_to_file = os.path.join(PATH_TO_DATA, 'Interval_indexes', filename)
+    #bounds_df = load_trial_bounds(path_to_file, dfs['rear_wheel'].index)
+    #static = merged[bounds_df.loc[0, 'start_time']:bounds_df.loc[0, 'stop_time']]
+    #static.plot(subplots=True, marker='.')
+    s = Session(session_label)
+    static = s.extract_trial('static')
+    static.loc[:, static.columns.str.contains('Accel')].plot(subplots=True, marker='.')
+    #static.interpolate(method='time').plot(subplots=True)
 
     plt.show()
