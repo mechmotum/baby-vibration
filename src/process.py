22bdd846421939e82889d3490149717b89e55543
Jason K. Moore <moorepants@gmail.com>
2024-12-11 15:06:07 +0100

    Constructed the first prototype of the final statistics table with mean speed as the only computed value.

--- src/process.py
+++ src/process.py
@@ -1,6 +1,8 @@
 import os
+from collections import defaultdict
 
 import yaml
+import pandas as pd
 
 from data import Session
 
@@ -12,9 +14,25 @@ with open(os.path.join(PATH_TO_REPO, 'data', 'sessions.yml')) as f:
 
 session_labels = list(session_meta_data.keys())
 
+motion_trials = ['stoeptegels']
+
+stats_table_data = defaultdict(list)
+
 for session_label in session_labels:
     print('Loading: ', session_label)
     s = Session(session_label)
     s.rotate_imu_data()
     print(s.trial_bounds)
+    s.calculate_travel_speed()
+    if 'stoeptegels' in s.trial_bounds:
+        for trial_num in s.trial_bounds['stoeptegels']:
+            trial_df = s.extract_trial('stoeptegels', trial_number=trial_num)
+            stats_table_data['Surface'].append('stoeptegels')
+            stats_table_data['Vehicle'].append(s.meta_data['vehicle'])
+            stats_table_data['Vehicle Type'].append(s.meta_data['vehicle_type'])
+            stats_table_data['Baby Age'].append(s.meta_data['baby_age'])
+            stats_table_data['Mean Speed'].append(trial_df['Speed'].mean())
+            del trial_df  # critical as this seems to be a copy!
     del s
+
+print(pd.DataFrame(stats_table_data))
