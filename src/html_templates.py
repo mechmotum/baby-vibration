f2c77c270997080db24ec7934a18de0058a114a1
Jason K. Moore <moorepants@gmail.com>
2024-12-20 10:57:49 +0100

    Added explanation of rotation check plots.

--- src/html_templates.py
+++ src/html_templates.py
@@ -28,7 +28,7 @@ INDEX = """
   <hr>
 {mean_table}
 
-  <h1>Box Plots of RMS of {signal} </h1>
+  <h1>Comparison Plots: RMS of {signal} </h1>
   <hr>
 {boxp_html}
 
@@ -52,14 +52,31 @@ INDEX = """
 
   <h1>Seat Pan Vertical Acceleration Spectrums</h1>
   <hr>
-{spect_html}
+{spec_html}
 
-  <h1>Sensor Rotations</h1>
+  <h1>Sensor Rotation Check</h1>
   <hr>
+  <p>
+    These plots give the information necessary to determine if the sensor
+    orientations are working correctly. These are time histories of the
+    "static" event (no motion) in each session. The left column shows the raw
+    accelerometer data for each sensor and each body fixed sensor axis. If the
+    background is shaded grey, that axis is the sensor's axis that aligned with
+    the vehicle's lateral axis (pitch axis) and the title on that subplot gives
+    the axis letter (x, y, or z) and the sign (axis points to the left - or
+    right +). The column on the right shows the accelerometer time histories
+    projected on the vehicle's body fixed axes: longitudinal, lateral, and
+    vertical. The vertical axis should show a positive ~10 m/s/s value and the
+    other two should axes should show approximately 0 m/s/s if the rotations
+    are applied correctly. This indicates that the vertical axes has been
+    aligned with gravity.
+  </p>
 {srot_html}
 
   <h1>Seat Pan Vertical Acceleration Time Histories</h1>
   <hr>
+  <p>
+  </p>
 {trial_html}
 
   </body>
